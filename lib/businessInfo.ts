16e4dc77dd997a6e273170ead05ab5200a104f12
hazichomwarr <batchiy6@gmail.com>
2026-05-12 10:44:51 -0400

    fix business phone number

--- lib/businessInfo.ts
+++ lib/businessInfo.ts
@@ -1,4 +1,4 @@
-export const BUSINESS_PHONE = "tel:+9175598350";
+export const BUSINESS_PHONE = "tel:+19175598350";
 export const BUSINESS_WHATSAPP = "https://wa.me/19175598350";
 export const BUSINESS_LOCATION =
   "https://www.google.com/maps/search/?api=1&query=216+Main+Street,+City+of+Orange,+NJ+07050";
